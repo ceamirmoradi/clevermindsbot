e60d409de71cb29c24accf46d798a9263b775663
ceamirmoradi <125783700+ceamirmoradi@users.noreply.github.com>
2026-07-23 21:14:29 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎭 به ربات Clever Minds خوش آمدید!\n\nربات با موفقیت فعال شد."
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()

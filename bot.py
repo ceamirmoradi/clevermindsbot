edd1cff00afb81f20b955695ba20df85b5f09659
ceamirmoradi <125783700+ceamirmoradi@users.noreply.github.com>
2026-07-26 22:10:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,37 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+TOKEN = os.getenv("BOT_TOKEN")
+PORT = int(os.getenv("PORT", "10000"))
+WEBHOOK_URL = "https://clevermindsbot.onrender.com"
+
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> None:
     await update.message.reply_text(
-        "🎭 خوش آمدید!\nربات Clever Minds با موفقیت فعال شد."
+        "💎 به ربات کلاب Clever Minds خوش آمدید!"
     )
 
-def main():
+
+def main() -> None:
     if not TOKEN:
         raise RuntimeError("BOT_TOKEN تنظیم نشده است")
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path="telegram",
+        webhook_url=f"{WEBHOOK_URL}/telegram",
+    )
+
 
 if __name__ == "__main__":
     main()

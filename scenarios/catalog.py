a8a78dd3614dec90cd3da383f5700bf56daa694d
ceamirmoradi <125783700+ceamirmoradi@users.noreply.github.com>
2026-07-27 20:29:03 +0200

    Add files via upload

--- scenarios/catalog.py
+++ scenarios/catalog.py
@@ -1,9 +1,17 @@
-from scenarios.aghrab import SCENARIO as AGHRAB_SCENARIO
-from scenarios.bazpors import SCENARIOS as BAZPORS_SCENARIOS
+from scenarios.bazpors import SCENARIO as BAZPORS_SCENARIO
 from scenarios.mozakere import SCENARIO as MOZAKERE_SCENARIO
+from scenarios.aghrab import SCENARIO as AGHRAB_SCENARIO
+from scenarios.shahnameh import SCENARIO as SHAHNAMEH_SCENARIO
+from scenarios.takavar import SCENARIO as TAKAVAR_SCENARIO
+from scenarios.kapo import SCENARIO as KAPO_SCENARIO
+from scenarios.namayande import SCENARIO as NAMAYANDE_SCENARIO
 
 SCENARIOS = {
-    **BAZPORS_SCENARIOS,
+    "bazpors": BAZPORS_SCENARIO,
     "mozakere": MOZAKERE_SCENARIO,
     "aghrab": AGHRAB_SCENARIO,
+    "shahnameh": SHAHNAMEH_SCENARIO,
+    "takavar": TAKAVAR_SCENARIO,
+    "kapo": KAPO_SCENARIO,
+    "namayande": NAMAYANDE_SCENARIO,
 }
